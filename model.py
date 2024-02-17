b042acc0fbdaa4d3d594a949f648d9d7d8932301
Maarten Groen <74355283+Maarten-Hub@users.noreply.github.com>
2024-02-17 23:24:04 +0100

    Removed useless model.py

--- model.py
+++
@@ -1,6 +0,0 @@
-import torch
-import torch.nn as nn
-
-model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
-model.eval()
-model.classifier[1] = nn.Linear(model.last_channel, 10)
